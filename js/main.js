1e55eb6298a2b0b1e21b6acc44a7702982777ce6
matlakh <73255302+matlakh@users.noreply.github.com>
2021-04-09 20:08:07 +0300

    чам

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const swiper = new Swiper('.swiper-container', {
+        loop: true,
+        loopedSlides: '1',
+        slidesPerView: 1,
+        autoplay: {
+            delay: 4000,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+            renderFraction: function (currentClass, totalClass) {
+                return '<div class="pagination">' +
+                    '<span class="' + currentClass + '"></span>' +
+                    '<img src="img/line.svg">' +
+                    '<span class="' + totalClass + '"></span>' + '</div>';
+            }
+        },
+        navigation: {
+            nextEl: '.right',
+            prevEl: '.left',
+        },
+
+
+    });
+
+})
