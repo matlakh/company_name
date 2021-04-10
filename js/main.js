83abfa8c28a3034fa63bcfdc59db15f6bb0c7991
matlakh <73255302+matlakh@users.noreply.github.com>
2021-04-10 19:34:29 +0300

    efw

--- js/main.js
+++ js/main.js
@@ -13,11 +13,15 @@ document.addEventListener("DOMContentLoaded", function () {
             clickable: true,
         },
         navigation: {
-            nextEl: '.arrow-right',
-            prevEl: '.arrow-left',
+            nextEl: '.arrow-left',
+            prevEl: '.arrow-right',
         },
-
-
     });
+    var  = new Swiper('.portfolio__swiper', {
+        slidesPerView: 4,
+        spaceBetween: 35,
+        pagination: false,
+      });
+
 
 })
